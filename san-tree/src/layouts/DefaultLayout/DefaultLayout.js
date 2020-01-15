025148087adc73f7acc8ca290f3bf17c9f4e13f8
SanTree <1468536322@qq.com>
2020-01-15 14:28:41 +0800

    配置搭建起步
    react框架搭建

---
+++ san-tree/src/layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import './DefaultLayout.less'
+export default class DefaultLayout extends Component{
+    render(){
+        return (
+            <div id="DefaultLayout">
+                HelloWorld
+            </div>
+        )
+    }
+}
